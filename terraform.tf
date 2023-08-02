04169943c392cc17d24f79983f9a8ce0f8046e2b
Massoud Valipoor <masval444@msn.com>
2023-08-02 12:37:54 +0200

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,19 +1,19 @@
-terraform {
+# terraform {
 
-  cloud {
-    organization = "organization-name"
+#   cloud {
+#     organization = "terraform__tutorial"
 
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
+#     workspaces {
+#       name = "learn-terraform-cloud"
+#     }
+#   }
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.28.0"
-    }
-  }
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.1.0"
+#     }
+#   }
 
-  required_version = ">= 0.14.0"
-}
+#   required_version = ">= 0.14.0"
+# }
